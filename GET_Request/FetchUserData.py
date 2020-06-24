5667ee619a67b31e7c9a0370e123dd4b3ae5a1e5
russel2005 <russel2005@gmail.com>
2020-06-24 13:30:20 -0400

    update GetchUserData.py

--- GET_Request/FetchUserData.py
+++ GET_Request/FetchUserData.py
@@ -3,10 +3,25 @@ import requests
 #API Url
 url = "https://reqres.in/api/users/?page=2"
 
-#Send Get Request
+# Send Get Request
 response = requests.get(url)
 
+
+# Display Response Content
 print(response.content)
+
+
+# Display response header
+print(response.headers)
+print(response.headers.get('Date'))
+print(response.headers.get('Server'))
+
+# Fetch Cookies
+print(response.cookies)
+
+# Fetch Encoding
+print(response.encoding)
+
 
 # if __name__ == '__main__':
 
